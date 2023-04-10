f4eaf0eb1edb29102fe641ea73ede38eb53573cd
Prateek Thakur <prateekthakur272@gmail.com>
2023-04-10 23:13:47 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
+        canvasColor: Colors.black
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
